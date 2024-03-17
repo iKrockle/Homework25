b6b15735e6c6ef981626b3626f5622318d31f508
Артем <forsakl123@gmail.com>
2024-03-17 15:36:04 +0500

    fixes

--- src/main/java/com/skyproHW25/Calculator/controller/CalculatorController.java
+++ src/main/java/com/skyproHW25/Calculator/controller/CalculatorController.java
@@ -11,33 +11,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
-    public CalculatorController(CalculatorService calculatorService){
+    public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
+
     @GetMapping
-    public String showHelloWorld(){return "Добро пожаловать в калькулятор";}
+    public String showHelloWorld() {
+        return "Добро пожаловать в калькулятор";
+    }
 
     @GetMapping("/plus")
     public String showPlus(@RequestParam("num1") Integer num1
-            ,@RequestParam("num2") Integer num2) {
-        return num1 +"+"+num2+"="+calculatorService.getPlus(num1,num2);
+            , @RequestParam("num2") Integer num2) {
+        return num1 + "+" + num2 + "=" + calculatorService.getPlus(num1, num2);
     }
 
     @GetMapping("/minus")
     public String showMinus(@RequestParam("num1") Integer num1
-            ,@RequestParam("num2") Integer num2) {
-        return num1 +"-"+num2+"="+calculatorService.getMinus(num1,num2);
+            , @RequestParam("num2") Integer num2) {
+        return num1 + "-" + num2 + "=" + calculatorService.getMinus(num1, num2);
     }
 
     @GetMapping("/multiply")
     public String showMultiply(@RequestParam("num1") Integer num1
-            ,@RequestParam("num2") Integer num2) {
-        return num1 +"*"+num2+"="+calculatorService.getMultiply(num1,num2);
+            , @RequestParam("num2") Integer num2) {
+        return num1 + "*" + num2 + "=" + calculatorService.getMultiply(num1, num2);
     }
 
     @GetMapping("/divide")
     public String showDivide(@RequestParam("num1") Integer num1
-            ,@RequestParam("num2") Integer num2)  {
-        return num1 +"/"+num2+"="+calculatorService.getDivide(num1,num2);
+            , @RequestParam("num2") Integer num2) {
+        return num1 + "/" + num2 + "=" + calculatorService.getDivide(num1, num2);
     }
 }
--- src/main/java/com/skyproHW25/Calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/skyproHW25/Calculator/service/CalculatorServiceImpl.java
@@ -5,26 +5,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
     @Override
-    public int getPlus(int num1,int num2) {
-        return num1+num2;
+    public int getPlus(int num1, int num2) {
+
+        return num1 + num2;
     }
 
     @Override
-    public int getMinus(int num1,int num2) {
-        return num1-num2;
+    public int getMinus(int num1, int num2) {
+        return num1 - num2;
     }
 
     @Override
-    public int getMultiply(int num1,int num2) {
-        return num1*num2;
+    public int getMultiply(int num1, int num2) {
+        return num1 * num2;
     }
 
     @Override
-    public double getDivide(int num1,int num2) {
-        if (num2==0){
+    public double getDivide(int num1, int num2) {
+        if (num2 == 0) {
             throw new IllegalArgumentException("Деление на 0 запрещено");
-        }else {
-            return (double) num1 /num2;
+        } else {
+            return (double) num1 / num2;
         }
     }
 }
